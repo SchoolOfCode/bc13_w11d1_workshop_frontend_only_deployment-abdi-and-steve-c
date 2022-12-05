8c802597fe65b2444d01bdacb9d8345f7e59a025
Abdiqani Mohamed <Mohamedabdiqani321@gmail.com>
2022-12-05 15:50:38 +0000

    checking again

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<h1>The first thing we have deployed!!</h1>
+				<h2>Checking for automatic deployment!!</h2>
 			</header>
 		</div>
 	);
